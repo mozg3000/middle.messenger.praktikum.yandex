2a54aca3b09041c8fa8aff7fd8072f24ecbeab9c
mozg3000 <lykosh@positron-it,ru>
2023-01-31 17:25:11 +0300

    feature. webpack base config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,12 @@
 const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './index.ts',
+  entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'project-name.bundle.js'
-  },
-  resolve: {
-    extensions: ['.ts', '.js', '.json']
+    filename: 'msgya.bundle.js'
   },
   module: {
     rules: [
@@ -19,11 +17,52 @@ module.exports = {
             loader: 'ts-loader',
             options: {
               configFile: path.resolve(__dirname, 'tsconfig.json'),
+              transpileOnly: true
             },
           },
         ],
+        resolve: {
+          extensions: ['.ts', '.js', '.json'],
+          enforceExtension: false,
+          fallback: {
+            fs: path.join(__dirname, 'node_modules') // @see https://github.com/handlebars-lang/handlebars.js/issues/953#issuecomment-94931306
+          },
+          alias: {
+            'handlebars': 'handlebars/runtime.js' // @see https://github.com/handlebars-lang/handlebars.js/issues/953#issuecomment-94931306
+          }
+        },
         exclude: /(node_modules)/
-      }
+      },
+      {
+        test: /\.handlebars$/,
+        loader: "handlebars-loader"
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
+                    "postcss-import",
+                    "postcss-simple-vars"
+                  ],
+                ],
+              },
+            },
+          },
+        ],
+      },
     ]
-  }
+  },
+  plugins: [
+    new ESLintPlugin({
+
+    })
+  ]
 };
