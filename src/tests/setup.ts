ee1410db1af5a4da89c83dee65bc9cb609130466
mozg3000 <lykosh@positron-it,ru>
2023-02-05 21:02:34 +0300

    edit. clean.

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,9 +1,3 @@
 import 'regenerator-runtime/runtime';
 import '@testing-library/jest-dom';
 import 'whatwg-fetch';
-import { server } from './apiMock';
-
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
-
